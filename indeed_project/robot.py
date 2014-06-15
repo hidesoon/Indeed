070f446d44c3ca02d6c79e6a5e8e5afdb977f01c
EliMor <sergf89@gmail.com>
2014-06-15 18:31:36 -0500

    email robot more generic

--- indeed_project/robot.py
+++ indeed_project/robot.py
@@ -3,10 +3,13 @@ from data_collector.extractNstore_T import file_grabber
 
 import robot_email
     # make a list that has robots in it and run vary by location
-def vbl(bigrammed=False):
+def vbl(bigrammed=False,with_email=False):
+    if with_email:
+	    params = robot_email.get_email_params()
     job_titles = file_grabber("job_titles.txt")
     robots = [Extraction_Robot(terms=title,with_bigrams=bigrammed) for title in job_titles]
     for robot in robots:
         robot.vary_by_locations()
-    message = "Hey! I'm done with the extraction. Come get your data."
-    robot_email.send(message) 
+    
+    if with_email:
+    	robot_email.send(*params) 
--- indeed_project/robot_email.py
+++ indeed_project/robot_email.py
@@ -2,16 +2,34 @@
 
 import smtplib
 
-def send(message):
-	try:
-		server = smtplib.SMTP('smtp.gmail.com','587')
-		server.ehlo()
-		server.starttls()
-		server.ehlo()
-		server.login('fromEmail@gmail.com','#####')
-		server.sendmail('fromEmail@gmail.com','toEmail@gmail.com',message)
-		server.close()
-	except:
-		print "Failed to send e-mail."
+def get_email_params():
+    to_email = ""
+    from_email = ""
+    from_email_pswd = ""
+    message = "Hey! I'm done with the extraction. Come get your data."
+    confirm = ""
+    while "y" not in confirm.lower():
+        to_email = raw_input("Enter e-mail address to send to: ")
+        from_email = raw_input("Enter e-mail address to send from: ")
+        from_email_pswd = raw_input("Enter password of e-mail address sending from: ")
+        print "Here's what I have: "
+        print "Sending to email: "+to_email
+        print "Sending from email: "+from_email
+        print "Password: " + from_email_pswd
+        confirm = raw_input("Is all this correct (y/n)?: ")
+    return (message,to_email,from_email,from_email_pswd)
 
-		
+
+def send(message,to_email,from_email,from_email_pswd):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com','587')
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(from_email,from_email_pswd)
+        server.sendmail(from_email,to_email,message)
+        server.close()
+    except:
+        print "Failed to send e-mail."
+
+
